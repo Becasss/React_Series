4fb65aece9f726addabcd84e2c68a0d9f8c2ddd8
w3schools-test <raeebikash@gmail.com>
2025-04-25 17:02:24 +0545

    Learning hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [counter , setCounter] =  useState(15) 
+  // let counter = 5
+  
+  const addValue = () =>
+  {
+    // counter = counter + 1
+    setCounter(counter + 1)
+    console.log("Value Added", counter);
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+    console.log("Clicked", counter)
+  }
+
+  return (
+    <>
+      <h1>Bikash Yamphu Rai</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button
+        onClick={addValue}>Add Value {counter}</button><br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  )
+}
+
+export default App
