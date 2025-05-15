c5830384df9219625ab9337a9d9a2e1aa5ec73ab
w3schools-test <raeebikash@gmail.com>
2025-05-15 16:58:09 +0545

    Learning basic of ReactJs

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,36 +1,37 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [counter , setCounter] =  useState(15) 
+  const [counter, setCounter] = useState(15);
   // let counter = 5
-  
-  const addValue = () =>
-  {
+
+  const addValue = () => {
     // counter = counter + 1
-    setCounter(counter + 1)
+    setCounter(counter + 1);
     console.log("Value Added", counter);
-  }
+  };
 
   const removeValue = () => {
-    setCounter(counter - 1)
-    console.log("Clicked", counter)
-  }
+    setCounter(counter - 1);
+    console.log("Clicked", counter);
+  };
 
   return (
     <>
       <h1>Bikash Yamphu Rai</h1>
       <h2>Counter Value : {counter}</h2>
 
-      <button
-        onClick={addValue}>Add Value {counter}</button><br />
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
       <button onClick={removeValue}>Remove Value {counter}</button>
-      <br /><br /><br />
-      <footer>Footer {counter} </footer><br />
+      <br />
+      <br />
+      <br />
+      <footer>Footer {counter} </footer>
+      <br />
       <button onClick={addValue}>Add Value {Counter - 1}</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const myObj = {
+    userName: "Bikash",
+    age: 28,
+  };
+  let newArr = [1, 2, 3];
+
   return (
-   
-    <><div class="py-8 px-8 max-w-sm mx-auto space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
-    <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/simple-react/public/pic3.jpg" alt="Woman's Face" />
-    <div class="text-center space-y-2 sm:text-left">
-      <div class="space-y-0.5">
-        <p class="text-lg text-black font-semibold">
-          Erin Lindford
-        </p>
-        <p class="text-slate-500 font-medium">
-          Product Engineer
-        </p>
-      </div>
-      <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-    </div>
-  </div>
-     
-   </>
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-5">
+        Bikash Yamphu Rai
+      </h1>
+      <Card username="Bikash Yamphu Rai" />
+      <Card username="Makalu Barun National Park" />
+    </>
   );
 }
 
